fa397d83f536209dad777cf6d10e0e2d98a5fd6b
Michael-mbogo <mikembogo17@gmail.com>
2018-06-25 16:24:22 +0300

    making api request

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SOURCE_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey={}&language=en'
+    SOURCE_API_BASE_URL = 'https://newsapi.org/v2/sources/{}?apiKey={}&language=en'
 
 
 
--- app/request.py
+++ app/request.py
@@ -1,4 +1,31 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 #Getting api_key
 api_key = app.config['SOURCE_API_KEY']
+
+#Getting source base url
+base_url = app.config['SOURCE_API_BASE_URL']
+
+
+def get_source(category):
+  """
+  This function gets json response to our url request
+  """
+  get_source_url = news_source_url.format(category)
+
+  with urllib.request.urlopen(get_source_url) as url:
+    get_source_data = url.read()
+    get_source_response = json.loads(get_source_data)
+
+    source_results = None
+
+    if get_source_response['sources']:
+      source_result_list =get_source_response['sources']
+
+      source_results = process_results(source_result_list)
+
+  return source_results
