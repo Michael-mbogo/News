aad0982d8f620dd3b566e1be4751afa4ee18948a
Michael-mbogo <mikembogo17@gmail.com>
2018-06-25 19:00:54 +0300

    Add macros

--- app/views.py
+++ app/views.py
@@ -9,25 +9,23 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    business_source = get_source('business')
+    entertainment_source = get_source('entertainment')
+    general_source = get_source('general')
+    health_source = get_source('health')
+    science_source = get_source('science')
+    sports_source = get_source('sports')
+    technology_source = get_source('technology')
 
-  business_source = get_source('business')
-  entertainment_source = get_source('entertainment')
-  international_source = get_source('international')
-  local_source = get_source('local')
-  technology_source = get_source('technology')
-  health_source = get_source('health')
-  weather_source = get_source('weather')
-  sport_source = get_source('sport')
-  # print(sport_source)
-  title = 'News Briefs'
-  return render_template('index.html', title = title, sport = sport_source,business = business_source, entertainment = entertainment_source, international = international_source, local = local_source, technology = technology_source, health = health_source, weather = weather_source)
+    title = 'News Briefs'
+    return render_template('index.html', title = title,business = business_source, entertainment = entertainment_source, general = general_source, health = health_source, science = science_source, sports = sports_source, technology = technology_source)
 
 
-# @app.route('/source/<id>')
-# def source(id):
-#
-#     articles = get_articles(id)
-#
-#     source_id = id
-#     title = f'{source_id}'
-#     return render_template('source.html',title = title,article = article)
+@app.route('/source/<id>')
+def source(id):
+
+    articles = get_articles(id)
+
+    source_id = id
+    title = f'{source_id}'
+    return render_template('source.html',title = title,article = article)
